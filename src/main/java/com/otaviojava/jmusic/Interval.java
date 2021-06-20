f16ceb38cc7e21ac8ca4d90dcdd1208b891ede1f
Otavio Santana <otaviopolianasantana@gmail.com>
2021-06-20 14:04:55 +0100

    creates fields in interval

--- src/main/java/com/otaviojava/jmusic/Interval.java
+++ src/main/java/com/otaviojava/jmusic/Interval.java
@@ -26,5 +26,26 @@ package com.otaviojava.jmusic;
  */
 public interface Interval {
 
+    /**
+     * In music, sharp, dièse (from French), or diesis (from Greek)[a] means higher in pitch.
+     * More specifically, in musical notation, sharp means "higher in pitch by one semitone (half step)".
+     * Sharp is the opposite of flat, which is a lowering of pitch.
+     *
+     * A sharp symbol, ♯, is used in key signatures or as an accidental.
+     * For instance, the music below has a key signature with three sharps
+     * (indicating either A major or F♯ minor, the relative minor) and the note, A♯, has a sharp accidental.
+     */
+    Interval SHARP = new Semitone(1);
+    /**
+     * In music, flat (Italian bemolle for "soft B") means "lower in pitch".
+     * Flat is the opposite of sharp, which is a raising of pitch. In musical notation, flat means
+     * "lower in pitch by one semitone (half step)", notated using the symbol ♭ which is derived from a
+     * stylised lowercase 'b'. For instance, the music below has a key signature with three flats
+     * (indicating either E♭ major or C minor) and the note, D♭, has a flat accidental.
+     */
+    Interval FLAT = new Semitone(-1);
+    Interval SEMITONE = new Semitone(1);
+    Interval TONE = new Semitone(2);
+
 
 }
