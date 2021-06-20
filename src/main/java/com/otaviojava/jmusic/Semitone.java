0bb3cddfb6500a9fdbb9a078a17e8279fa92cc1e
Otavio Santana <otaviopolianasantana@gmail.com>
2021-06-20 10:58:21 +0100

    creates semitone instance

--- src/main/java/com/otaviojava/jmusic/Semitone.java
+++ src/main/java/com/otaviojava/jmusic/Semitone.java
@@ -1,2 +1,29 @@
-package com.otaviojava.jmusic;public interface Semitone {
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.otaviojava.jmusic;
+
+/**
+ * A semitone, also called a half step or a half tone,[3] is the smallest musical interval commonly used
+ * in Western tonal music,[4] and it is considered the most dissonant[5] when sounded harmonically.
+ * It is defined as the interval between two adjacent notes in a 12-tone scale. For example,
+ * C is adjacent to C♯; the interval between them is a semitone.
+ */
+public interface Semitone {
 }
