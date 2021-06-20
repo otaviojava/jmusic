40a86027dec0445bf937e0add71590cdac538293
Otavio Santana <otaviopolianasantana@gmail.com>
2021-06-20 08:21:38 +0100

    creates package info file

---
+++ src/main/java/com/otaviojava/jmusic/package-info.java
@@ -0,0 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * Music theory is the study of the practices and possibilities of music. The Oxford Companion to Music describes three
+ * interrelated uses of the term "music theory". The first is the "rudiments", that are needed to understand music
+ * notation (key signatures, time signatures, and rhythmic notation); the second is learning scholars' views on music
+ * from antiquity to the present; the third a sub-topic of musicology that "seeks to define processes and general
+ * principles in music". The musicological approach to theory differs from music analysis "in that it takes as its
+ * starting-point not the individual work or performance but the fundamental materials from which it is built."
+ */
+package com.otaviojava.jmusic;
