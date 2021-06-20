577b00a5748e68e3e1e72f64beb8bf8da87cad93
Otavio Santana <otaviopolianasantana@gmail.com>
2021-06-20 11:12:14 +0100

    creates notes class

--- src/main/java/com/otaviojava/jmusic/Notes.java
+++ src/main/java/com/otaviojava/jmusic/Notes.java
@@ -20,11 +20,15 @@
 package com.otaviojava.jmusic;
 
 /**
- * A semitone, also called a half step or a half tone,[3] is the smallest musical interval commonly used
- * in Western tonal music,[4] and it is considered the most dissonant[5] when sounded harmonically.
- * It is defined as the interval between two adjacent notes in a 12-tone scale. For example,
- * C is adjacent to C♯; the interval between them is a semitone.
+ * An utilitarian class to {@link Note}
  */
-public final class Semitone implements Interval {
+public class Notes {
 
+    public enum EnglishConvention {
+        C, D, E, F, G, A, B;
+    }
+
+    public enum NeoLatinConvention {
+        DO, RE, MI, FA, SOL, LA, SI
+    }
 }
